f26b5c807d100da52fd4e86d6b54e4c0901be3b1
Alexander Tumanin <alexander.tumanin@check24.de>
2020-02-24 10:34:18 +0100

    new fragment

---
+++ app/src/main/java/com/example/testcodeclimateapp/ui/AppFragment.kt
@@ -0,0 +1,9 @@
+package com.example.testcodeclimateapp.ui
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by ATumanin on 24.02.2020.
+ */
+open class AppFragment : Fragment(){
+}
--- app/src/main/java/com/example/testcodeclimateapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/testcodeclimateapp/ui/gallery/GalleryFragment.kt
@@ -9,8 +9,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.testcodeclimateapp.R
+import com.example.testcodeclimateapp.ui.AppFragment
 
-class GalleryFragment : Fragment() {
+class GalleryFragment : AppFragment() {
 
     private lateinit var galleryViewModel: GalleryViewModel
 
