654688db11a4df15fd210c8b802ba932b3ea4d79
Alexander Tumanin <alexander.tumanin@check24.de>
2020-02-24 11:27:58 +0100

    add code complexity

--- app/src/main/java/com/example/testcodeclimateapp/ui/AppFragment.kt
+++ app/src/main/java/com/example/testcodeclimateapp/ui/AppFragment.kt
@@ -6,4 +6,30 @@ import androidx.fragment.app.Fragment
  * Created by ATumanin on 24.02.2020.
  */
 open class AppFragment : Fragment(){
+
+    val count = 10
+    var anotherCOUNT: Int? = null
+    var name = "name"
+    var lastname: String? = null
+
+    fun letCount(): Int {
+        var newInt = 0
+        if(anotherCOUNT ?: 0 > 0) {
+            newInt = 1
+            if(lastname?.equals("lastname") == true) {
+                newInt = 2
+            }
+            if(lastname?.equals("name") == true) {
+                newInt = 3
+            }
+        } else {
+            newInt = 4
+        }
+        when (newInt) {
+            0 -> return 5
+            1 -> return 6
+            2 -> return 7
+            else -> return 8
+        }
+    }
 }
